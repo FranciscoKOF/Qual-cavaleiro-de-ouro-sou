aec2ce8009c42cdbe9935268684134927ec67c81
Francisco Junior <franciscocnascimentojr@gmail.com>
2022-08-21 14:17:55 -0300

    alteracao

--- Qual cavaleiro de ouro sou/Program.cs
+++ Qual cavaleiro de ouro sou/Program.cs
@@ -12,7 +12,7 @@ namespace Qual_cavaleiro_de_ouro_sou
         static void Main(string[] args)
         {
             bool terminarPrograma = false;
-            
+
 
             while (!terminarPrograma)
             {
@@ -23,70 +23,96 @@ namespace Qual_cavaleiro_de_ouro_sou
                 Console.WriteLine("Digite o dia do seu aniversario");
                 int.TryParse(Console.ReadLine(), out int dia);
 
+                while(dia > 31)
+                {
+                    Console.WriteLine("Dia invalido. Digite o dia do seu aniversario");
+                    int.TryParse(Console.ReadLine(), out int dia2);
+
+                    dia = dia2;
+                }
+
                 Console.WriteLine("Agora o mes representado pelo seu numero");
                 int.TryParse(Console.ReadLine(), out int mes);
 
+                while (mes > 12)
+                {
+                    Console.WriteLine("Mes invalido. Digite o mes do seu aniversario");
+                    int.TryParse(Console.ReadLine(), out int mes2);
 
+                    mes = mes2;
+                }
 
-
-                if ((dia >= 21 && mes == 3) || (dia <= 20 && mes == 4))
-                {
-                    Console.WriteLine("Áries");
-                }
-                if ((dia >= 21 && mes == 4) || (dia <= 20 && mes == 5))
-                {
-                    Console.WriteLine("Touro");
-                }
-                if ((dia >= 21 && mes == 5) || (dia <= 20 && mes == 6))
-                {
-                    Console.WriteLine("Gêmeos");
-                }
-                if ((dia >= 21 && mes == 6) || (dia <= 22 && mes == 7))
-                {
-                    Console.WriteLine("Câncer");
-                }
-                if ((dia >= 23 && mes == 7) || (dia <= 22 && mes == 8))
-                {
-                    Console.WriteLine("Leão");
-                }
-                if ((dia >= 23 && mes == 8) || (dia <= 22 && mes == 9))
-                {
-                    Console.WriteLine("Virgem");
-                }
-                if ((dia >= 23 && mes == 9) || (dia <= 222 && mes == 10))
-                {
-                    Console.WriteLine("Libra");
-                }
-                if ((dia >= 23 && mes == 10) || (dia <= 21 && mes == 11))
-                {
-                    Console.WriteLine("Escorpião");
-                }
-                if ((dia >= 22 && mes == 11) || (dia <= 21 && mes == 12))
-                {
-                    Console.WriteLine("Sagitário");
-                }
-                if ((dia >= 22 && mes == 12) || (dia <= 20 && mes == 1))
-                {
-                    Console.WriteLine("Capricórnio");
-                }
-                if ((dia >= 21 && mes == 1) || (dia <= 18 && mes == 2))
-                {
-                    Console.WriteLine("Aquário");
-                }
-                if ((dia >= 19 && mes == 2) || (dia <= 20 && mes == 3))
-                {
-                    Console.WriteLine("Peixes");
-                }
+                Console.WriteLine(RetornaCavaleiro(dia, mes));
 
                 Console.WriteLine("Digite 1 pra sair ou 2 para continuar");
-                terminarPrograma = int.Parse(Console.ReadLine()) == 1;
+                try
+                {
+                    terminarPrograma = int.Parse(Console.ReadLine()) == 1;
+
+                }
+                catch (Exception)
+                {
+
+                    
+                }
                 
-
             }
-            
-            
-                         
+        }
 
+        private static string RetornaCavaleiro(int dia, int mes)
+        {
+            string cavaleiro = "";
+
+            if ((dia >= 21 && mes == 3) || (dia <= 20 && mes == 4))
+            {
+                cavaleiro = "Áries";
+            }
+            if ((dia >= 21 && mes == 4) || (dia <= 20 && mes == 5))
+            {
+                cavaleiro = "Touro";
+            }
+            if ((dia >= 21 && mes == 5) || (dia <= 20 && mes == 6))
+            {
+                cavaleiro = "Gêmeos";
+            }
+            if ((dia >= 21 && mes == 6) || (dia <= 22 && mes == 7))
+            {
+                cavaleiro = "Câncer";
+            }
+            if ((dia >= 23 && mes == 7) || (dia <= 22 && mes == 8))
+            {
+                cavaleiro = "Leão";
+            }
+            if ((dia >= 23 && mes == 8) || (dia <= 22 && mes == 9))
+            {
+                cavaleiro = "Virgem";
+            }
+            if ((dia >= 23 && mes == 9) || (dia <= 222 && mes == 10))
+            {
+                cavaleiro = "Libra";
+            }
+            if ((dia >= 23 && mes == 10) || (dia <= 21 && mes == 11))
+            {
+                cavaleiro = "Escorpião";
+            }
+            if ((dia >= 22 && mes == 11) || (dia <= 21 && mes == 12))
+            {
+                cavaleiro = "Sagitário";
+            }
+            if ((dia >= 22 && mes == 12) || (dia <= 20 && mes == 1))
+            {
+                cavaleiro = "Capricórnio";
+            }
+            if ((dia >= 21 && mes == 1) || (dia <= 18 && mes == 2))
+            {
+                cavaleiro = "Aquário";
+            }
+            if ((dia >= 19 && mes == 2) || (dia <= 20 && mes == 3))
+            {
+                cavaleiro = "Peixes";
+            }
+
+            return cavaleiro;
         }
     }
 }
